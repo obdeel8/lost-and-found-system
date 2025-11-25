e7b9b674663201c96601d32744b16c09a59bc04c
obdeel8 <deelan.okati@strathmore.edu>
2025-11-25 13:04:46 +0300

    Add main application class

---
+++ src/main/java/com/university/lostandfound/LostAndFoundApplication.java
@@ -0,0 +1,10 @@
+package com.university.lostandfound;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LostAndFoundApplication.class, args);
+    }
+}
